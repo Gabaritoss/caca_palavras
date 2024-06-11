7596655d9df7b2ac8ee99cc487b907142c2b8455
Gabaritoss <167150798+Gabaritoss@users.noreply.github.com>
2024-06-11 13:10:19 -0300

    Add files via upload
    função caca_palavras e coluna com share de palavras

--- main.py
+++ main.py
@@ -11,16 +11,66 @@ lista_planilhas = []
 arquivo_usuario = []
 novo_arquivo = 0
 
-# armazenar nome dos arquivos
+# Função caca palavras
+
+def caça_palavras(data_frame, planilha):
+    """
+    Função para analisar um DataFrame de descrições, 
+    limpar os textos, contar as palavras repetidas e 
+    salvar os resultados em um arquivo Excel.
+
+    Argumentos:
+        data_frame (pandas.DataFrame): DataFrame com a coluna 'DESCRIPCION'.
+        planilha (str): Nome do arquivo de saída.
+    """
+
+    frases = data_frame['DESCRIPCION'].apply(str) # Armazena a coluna descripcion na variavel frases
+
+    # Limpeza do descritivo
+
+    frases_sem_numeros = [re.sub(r'[0-9]', '', frase) for frase in frases] # remove numeros
+    frases_sem_numeros_maiusculas = [frase.upper() for frase in frases_sem_numeros] # converte em maiusculo
+    frases_sem_kg_fat = [re.sub(r'\bKG\b|\bFAT\b|\bDE\b|[.,-:;%+&\*()]', ' ', frase) for frase in frases_sem_numeros_maiusculas] # remove kg e fat
+    frases_limpas = [re.sub(r'\b\w\b|\b\w\w\b', '', frase) for frase in frases_sem_kg_fat] # remove palavras com 2 ou menos caracteres
+
+    # Dicionário para armazenar palavras repetidas
+
+    palavras_repetidas = {}
+    for frases_limpas in frases_limpas:
+        for palavra in frases_limpas.split():
+            if palavra in palavras_repetidas:
+                palavras_repetidas[palavra] += 1
+            else:
+                palavras_repetidas[palavra] = 1
+
+    # Criação do data frame
+
+    df_palavras_repetidas = pd.DataFrame.from_dict(palavras_repetidas, orient='index', columns=['Contagem'])
+    df_palavras_repetidas.reset_index(inplace=True)
+    df_palavras_repetidas.columns = ['Palavra', 'Contagem']
+    
+    # Cálculos
+
+    Contagem = df_palavras_repetidas["Contagem"]
+
+    df_palavras_repetidas['Share'] = Contagem / Contagem.sum() # Calcula o share para cada palavra
+
+    # Criação do arquivo excel
+
+    ## IMPORTANTE: colocar o caminho completo da planilha
+    df_palavras_repetidas.to_excel(f'C:/Users/Gabriel/Desktop/caça_palavras/arquivos_finais/{planilha}_final.xlsx', index=False)
+    print(f"arquivo {planilha}_final criado com sucesso")
+
+# armazenar nome dos arquivos determinados pelo usuario
 
 while novo_arquivo != "":
-  novo_arquivo = input("insira o nome das planilhas ou aperte enter para finalizar: ")
-  arquivo_usuario.append(f"{novo_arquivo}")
+  novo_arquivo = input("insira o nome das planilhas ou aperte enter para finalizar: ") # Solicita os arquivos
+  arquivo_usuario.append(f"{novo_arquivo}") # Adiciona eles em uma lista
   print(f"{novo_arquivo} adicionado.")
   
-arquivo_usuario.remove("")
-print(f"lista de planilhas carregadas: {arquivo_usuario}")
-lista_planilhas += list(arquivo_usuario)
+arquivo_usuario.remove("") # Remove o espaço em branco do enter, condição para sair do loop while
+print(f"lista de planilhas carregadas: {arquivo_usuario}") # Retorna a lista de arquivos carregados
+lista_planilhas += list(arquivo_usuario) # Adiciona os arquivos do usuario a lista de planilhas
 
 # Executa algoritmo na lista de planilhas
 
@@ -31,44 +81,10 @@ for planilha in lista_planilhas:
     ## IMPORTANTE: colocar o caminho completo da planilha
 
     try:
-        data_frame = pd.read_excel(f"C:/Users/Gabriel/Desktop/caça_palavras/arquivos_iniciais/{planilha}.xlsx")
+        data_frame = pd.read_excel(f"C:/Users/Gabriel/Desktop/caça_palavras/arquivos_iniciais/{planilha}.xlsx") # Le o arquivo excel do usuario
         
     except FileNotFoundError:
-        print(f"arquivo {planilha} não encontrado, revise o nome do arquivo e garanta que esta em formato .xlsx.")
+        print(f"arquivo {planilha} não encontrado, revise o nome do arquivo e garanta que esta em formato .xlsx.") # Sinaliza caso haja algum erro na etapa anterior
     
     else:
-
-        # Determina a coluna que o arquivo vai ler
-      
-        frases = data_frame['DESCRIPCION'].apply(str)
-        
-        # limpeza do descritivo
-
-        frases_sem_numeros = [re.sub(r'[0-9]', '', frase) for frase in frases]
-        frases_sem_numeros_maiusculas = [frase.upper() for frase in frases_sem_numeros]
-        frases_sem_kg_fat = [re.sub(r'\bKG\b|\bFAT\b|\bDE\b|[.,-:;%+&\*()]|\b\w\w\b', ' ', frase) for frase in frases_sem_numeros_maiusculas]
-        frases_sem_palavras_curtas = [re.sub(r'\b\w\b', '', frase) for frase in frases_sem_kg_fat]
-
-        # Dicionário para armazenar palavras repetidas
-
-        palavras_repetidas = {}
-        for frase_sem_palavra_curta in frases_sem_palavras_curtas:
-            for palavra in frase_sem_palavra_curta.split():
-                if palavra in palavras_repetidas:
-                    palavras_repetidas[palavra] += 1
-                else:
-                    palavras_repetidas[palavra] = 1
-
-        # Criação do data frame
-
-        df_palavras_repetidas = pd.DataFrame.from_dict(palavras_repetidas, orient='index', columns=['Contagem'])
-        df_palavras_repetidas.reset_index(inplace=True)
-        df_palavras_repetidas.columns = ['Palavra', 'Contagem']
-        
-        # Criação do arquivo excel
-
-        ## IMPORTANTE: colocar o caminho completo da planilha
-
-        df_palavras_repetidas.to_excel(f'C:/Users/Gabriel/Desktop/caça_palavras/arquivos_finais/{planilha}_final.xlsx', index=False)
-        
-        print(f"arquivo {planilha}_final criado com sucesso")
+        caça_palavras(data_frame, planilha) # Executa a função caça_palavras com os arquivos do usuario.
